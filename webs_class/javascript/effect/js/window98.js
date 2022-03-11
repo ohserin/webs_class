3aa05ef22e7542d73ade61919d6fe4a45ad1d7c7
ohserin <dhtpfls321@gmail.com>
2022-03-11 17:43:49 +0900

    22.03.11

--- webs_class/javascript/effect/js/window98.js
+++ webs_class/javascript/effect/js/window98.js
@@ -1,45 +1,49 @@
 //highlight
-hljs.highlightAll();
+const highlight = function(){
+    hljs.highlightAll()
+}
 
-//modal
-const modal = function () {
-    //버튼을 클릭하면 modal창을 보이게 해주세요.
-    document.querySelector(".source-btn").addEventListener("click", () => {
-        document.querySelector("#modal").classList.add("show")
-        //닫기버튼 눌렀을 때 hide를 지워 지속적으로 버튼 사용가능하게
-        document.querySelector("#modal").classList.remove("hide")
-    })
-    // document.querySelector(".modal-close").addEventListener("click", () => {
-    //     document.querySelector("#modal").classList.add("hide")
-    // })
+//윈도우 오픈
+function openWindow() {
+    document.querySelector(".quiz_source").addEventListener("click", () => {
+        document.querySelector(".source").classList.add("show");
+        document.querySelector(".source .window").classList.add("show");
+        document.querySelector(".source").classList.remove("hide");
+        document.querySelector(".source .window").classList.remove("hide");
+    });
     document.querySelector(".close").addEventListener("click", () => {
-        document.querySelector("#modal").classList.add("hide")
+        document.querySelector(".source").classList.add("hide");
+        document.querySelector(".source .window").classList.add("hide");
+        document.querySelector(".source").classList.remove("show");
+        document.querySelector(".source .window").classList.remove("show");
+    });
+    document.querySelector(".minimize").addEventListener("click", () => {
+        document.querySelector(".source").classList.add("hide");
+        document.querySelector(".source .window").classList.add("hide");
+        document.querySelector(".source").classList.remove("show");
+        document.querySelector(".source .window").classList.remove("show");
+    });
+}
+openWindow();
+    
+
+//tab 메뉴
+const tabMenu = function(){
+    const tabBtn = document.querySelectorAll(".menu-bar > span");
+    const tabCont = document.querySelectorAll(".main > div");
+
+    tabBtn.forEach((btn, index) => {
+        btn.addEventListener("click", () => {
+            tabBtn.forEach(el => {
+                el.classList.remove("active");
+            });
+            btn.classList.add("active");
+
+            tabCont.forEach(e => {
+                e.style.display = "none";
+            })
+        tabCont[index].style.display = "block";
+        })
     })
 }
-modal()
-       
-    const tabMenu = function () {
-        const viewBtn = document.querySelectorAll(".menu-bar > span")
-        const viewCont = document.querySelectorAll(".view-cont > div")
-    
-        viewBtn.forEach((btn, index) => {
-            btn.addEventListener("click", () => {
-                // 모든 클래스 클릭하지 않은건 active 삭제
-                for (let i of viewBtn) {
-                    i.classList.remove("active")
-                }
-                //내가 클릭한건 active 추가
-                btn.classList.add("active")
-    
-                //모든 컨텐츠 박스 안보이게
-                viewCont.forEach(el => {
-                    el.style.display = "none";
-                })
-                viewCont[index].style.display = "block"
-    
-                //내가 클릭한 콘텐츠 박스를 보이게
-    
-            })
-        })
-    }
-    tabMenu()
+tabMenu();
