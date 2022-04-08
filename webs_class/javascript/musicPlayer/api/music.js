212c2f0c873741e5de86ce8ce4751f15d0583522
yuram2 <lur0872@gmail.com>
2022-04-08 10:22:00 +0900

    22.04.08

--- webs_class/javascript/musicPlayer/api/music.js
+++ webs_class/javascript/musicPlayer/api/music.js
@@ -3,119 +3,119 @@ let allMusic = [
         name : "노래 제목 - 제목",
         artist : "Jobe Mickal",
         img : "music-1",
-        audio :"mousic-1"
+        audio :"music-1"
     },{
         name : "노래 제목 - 제목",
         artist : "Judy Garland",
         img : "music-2",
-        audio :"mousic-2"
+        audio :"music-2"
     },
     {
         name : "노래 제목 - 제목",
         artist : "Justin Drew Bieber",
         img : "music-3",
-        audio :"mousic-3"
+        audio :"music-3"
     },
     {
         name : "노래 제목 - 제목",
         artist : "Edward Christopher",
         img : "music-4",
-        audio :"mousic-4"
+        audio :"music-4"
     },
     {
         name : "노래 제목 - 제목",
         artist : "Charlie Puth",
         img : "music-5",
-        audio :"mousic-5"
+        audio :"music-5"
     },
     {
         name : "노래 제목 - 제목",
         artist : "Ariana Grande",
         img : "music-6",
-        audio :"mousic-6"
+        audio :"music-6"
     },
     {
         name : "노래 제목 - 제목",
         artist : "Lady Gaga",
         img : "music-7",
-        audio :"mousic-7"
+        audio :"music-7"
     },
     {
         name : "You are not alone",
         artist : "Michael Joseph Jackson",
         img : "music-8",
-        audio :"mousic-8"
+        audio :"music-8"
     },
     {
         name : "노래 제목 - 제목",
         artist : "Jobe Mickal",
         img : "music-9",
-        audio :"mousic-9"
+        audio :"music-9"
     },
     {
         name : "노래 제목 - 제목",
         artist : "Jobe Mickal",
         img : "music-10",
-        audio :"mousic-10"
+        audio :"music-10"
     },
     {
         name : "노래 제목 - 제목",
         artist : "Jobe Mickal",
         img : "music-11",
-        audio :"mousic-11"
+        audio :"music-11"
     },
     {
         name : "노래 제목 - 제목",
         artist : "Jobe Mickal",
         img : "music-12",
-        audio :"mousic-12"
+        audio :"music-12"
     },
     {
         name : "노래 제목 - 제목",
         artist : "Jobe Mickal",
         img : "music-13",
-        audio :"mousic-13"
+        audio :"music-13"
     },
     {
         name : "노래 제목 - 제목",
         artist : "Jobe Mickal",
         img : "music-14",
-        audio :"mousic-14"
+        audio :"music-14"
     },
     {
         name : "노래 제목 - 제목",
         artist : "Jobe Mickal",
         img : "music-15",
-        audio :"mousic-15"
+        audio :"music-15"
     },
     {
         name : "노래 제목 - 제목",
         artist : "Jobe Mickal",
         img : "music-16",
-        audio :"mousic-16"
+        audio :"music-16"
     },
     {
         name : "노래 제목 - 제목",
         artist : "Jobe Mickal",
         img : "music-17",
-        audio :"mousic-17"
+        audio :"music-17"
     },
     {
         name : "노래 제목 - 제목",
         artist : "Jobe Mickal",
         img : "music-18",
-        audio :"mousic-18"
+        audio :"music-18"
     },
     {
         name : "노래 제목 - 제목",
         artist : "Jobe Mickal",
         img : "music-19",
-        audio :"mousic-19"
+        audio :"music-19"
     },
     {
         name : "노래 제목 - 제목",
         artist : "Jobe Mickal",
         img : "music-20",
-        audio :"mousic-20"
+        audio :"music-20"
     }
 ]
